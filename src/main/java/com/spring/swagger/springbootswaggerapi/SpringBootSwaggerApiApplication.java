692f369a07bea4bf1992734e5d34d98621a1c61c
Feven <feven.h.mamo@gmail.com>
2020-05-11 04:41:10 -0500

    spring swagger documentation

---
+++ src/main/java/com/spring/swagger/springbootswaggerapi/SpringBootSwaggerApiApplication.java
@@ -0,0 +1,44 @@
+package com.spring.swagger.springbootswaggerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@SpringBootApplication
+@EnableSwagger2
+public class SpringBootSwaggerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSwaggerApiApplication.class, args);
+    }
+
+    @Bean
+    public Docket swaggerConfiguration(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.ant("/api/*"))
+                .apis(RequestHandlerSelectors.basePackage("com.spring.swagger"))
+                .build()
+                .apiInfo(apiDetails());
+    }
+
+    private ApiInfo apiDetails(){
+        return new ApiInfo(
+                "Address book API",
+                "Sample API for swagger demonistration",
+                "1.0",
+                "Free to use",
+                new springfox.documentation.service.Contact("Feven Mamo", "http://mywebsite.com","abc@gmail.com"),
+                "API License",
+                "http://mywebsite.com",
+                Collections.emptyList());
+    }
+}
